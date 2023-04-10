5524c11dadebe7aae8375ee9cc584a3982a05a3f
shivrjpatil <shivrajp972@gmail.com>
2023-04-10 19:15:35 +0530

    Added calculating the length of two lines

--- LineComparison274/Program.cs
+++ LineComparison274/Program.cs
@@ -11,6 +11,18 @@ namespace LineComparison274
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to linecomparison program");
+            Console.WriteLine("enter the X1 and Y1 coordinate");
+            double X1 = Convert.ToDouble(Console.ReadLine());
+            double Y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter the X2 and Y2 coordinate");
+            double X2 = Convert.ToDouble(Console.ReadLine());
+            double Y2 = Convert.ToDouble(Console.ReadLine());
+
+            double Length = Math.Sqrt((Math.Pow((X2 - X1), 2)) + (Math.Pow((Y2 - Y1), 2)));
+            Console.WriteLine("the lenght of two line " + Length);
+
+            Console.ReadLine();
         }
     }
 }
